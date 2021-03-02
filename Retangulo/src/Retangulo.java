1a0b9c2eaa25dfe798cd67c9cc804b4a43168ec9
Sara <sara.silvadefaria@hotmail.com>
2021-03-02 11:23:21 -0300

    aula 03-02-2021 - Convensor de temperatura

---
+++ Retangulo/src/Retangulo.java
@@ -0,0 +1,19 @@
+
+public class Retangulo {
+
+	public static void main(String[] args) {
+		
+		double base =8;
+		double altura = 5;
+		double area;
+		double perimetro;
+		
+		area = base * altura;
+		perimetro = base + base + altura +altura;
+		
+		System.out.println("Área: " + area + " metros");
+		System.out.print("Perímetro: " + perimetro + " metros");
+
+	}
+
+}
---
+++ temperatura/src/temperatura/Celsius.java
@@ -0,0 +1,13 @@
+package temperatura;
+
+public class Celsius {
+	
+	public static void main(String[] args) {
+		
+		double C = 35.0;
+		double F = (9 * C + 160) / 5;
+		
+		System.out.print(C + " graus Celcius" + "é igual a " + F + " Fahrenheit");
+	}
+
+}
